1e532f984a061abd02a619b78ea37d33048300ec
Ali Al Dobyan <ali.aldobyan@gmail.com>
2020-08-27 14:09:43 +0300

    task 3 is created and working

--- restaurants/views.py
+++ restaurants/views.py
@@ -5,7 +5,19 @@ def welcome(request):
 
 def restaurant_list(request):
 
-    context = {
+    context = {"my_list": [
+            {
+
+                "restaurant_name":'McDonald\'s',
+                "food_type":'american food'
+            },
+            {
+                "restaurant_name" : 'Roma Restaurant',
+                "food_type": 'italian food'
+
+            }
+
+        ],
 
     }
     return render(request, 'list.html', context)
@@ -13,7 +25,11 @@ def restaurant_list(request):
 
 def restaurant_detail(request):
 
-    context = {
+    context = {"my_object":
+        {
+            "restaurant_name": 'McDonald\'s' ,
+            "food_type" : 'american food'
 
+        }
     }
     return render(request, 'detail.html', context)
